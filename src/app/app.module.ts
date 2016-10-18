722310a4e41b554a8567f4b44a66d52c14d5acbf
Greg Kedzierski <greg@gregkedzierski.com>
2016-10-18 15:40:56 +0100

    Add CityPhoto component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CityPhotoComponent } from './city-photo/city-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityPhotoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/city-photo/city-photo.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CityPhotoComponent } from './city-photo.component';
+
+describe('Component: CityPhoto', () => {
+  it('should create an instance', () => {
+    let component = new CityPhotoComponent();
+    expect(component).toBeTruthy();
+  });
+});
